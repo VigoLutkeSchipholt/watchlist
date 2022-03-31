2d4dfa10bee694b98557d9db78d42f7b8494dd71
VigoLutkeSchipholt <91940746+VigoLutkeSchipholt@users.noreply.github.com>
2022-03-31 13:52:51 +0200

    Add files via upload

---
+++ watchlist/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Watchlist</title>
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+    <div class="header">
+        <p> </p>
+
+
+    </div>
+    <div class="planeten">
+
+        <img src="ice.png" height="150" width="150" class="space">
+        <img src="earth.png" height="150" width="200" class="space">
+        <img src="blueplaneten.png" height="150" width="200" class="space">
+        <img src="planet.png" height="150" width="200" class="space">
+        <div>
+
+        </div>
+    </div>
+
+    <div class="footer">
+        <p>©Vigolutkeschipholt </p>
+    </div>
+
+</body>
+
+</html>
